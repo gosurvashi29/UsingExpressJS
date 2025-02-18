764057a0007b756c17fa5fc11e26efef58f27fc4
gosurvashi29 <urvashigoswami350@gmail.com>
2025-02-18 09:51:56 +0530

    using body parser

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 const http = require("http");
 //const fs = require("fs");
 const express=require("express")
+const bodyParser= require('body-parser');
 
 const app= express();
 
-app.use((req,res,next)=>{
-    console.log('In the middleware!');
-    next();
-})
+app.use(bodyParser.urlencoded({extended : false}));
 
-app.use((req,res,next)=>{
-    console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
-})
 
+
+app.use('/add-product',(req,res,next)=>{
+    //console.log('In the middleware!');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"></input><button type="submit">Add</button></form>');
+});
+
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/',(req,res,next)=>{
+    res.send('<h1>Hello from ExpressJS!</h1>')
+});
 app.listen(3000); //does work of line no. 18-23.
 // const server = http.createServer(routes.requestHandler)
 
